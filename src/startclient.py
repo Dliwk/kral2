6cc65affba9bc264011aa4e1fa03bd77e5246792
Roman Trapeznikov <roma190505@yandex.ru>
2020-06-16 19:47:56 +0300

    client configuration support

--- src/startclient.py
+++ src/startclient.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2020 Roman Trapeznikov
 
+import os
+import yaml
 from kral2.client.network import LocalClient
 from kral2.client.graphics.thread import GraphicsThread
 
 if __name__ == '__main__':
-    client = LocalClient('127.0.0.1', 43210, name='Player')
+    config = {'ip': '127.0.0.1', 'port': 43210, 'name': 'Player'}
+    if os.path.exists('client_config.yaml'):
+        with open('client_config.yaml') as conf:
+            data = conf.read()
+        config = yaml.load(data, yaml.Loader)
+    client = LocalClient(config['ip'], config['port'], name=config['name'])
     gt = GraphicsThread(client)
     gt.start()
